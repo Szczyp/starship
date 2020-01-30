65a8fb6ca087b6dcc6b89d37905d0c41cfe3c5f8
Szczyp <qb@szczyp.com>
2020-01-30 16:42:10 +0100

    Improve haskell module to detect .cabal and .hs file extensions.

--- src/modules/haskell.rs
+++ src/modules/haskell.rs
@@ -6,35 +6,36 @@ use crate::utils;
 /// Creates a module with the current Haskell Stack version
 ///
 /// Will display the Haskell version if any of the following criteria are met:
+///     - Current directory contains a file with a `.cabal`, `.hs`, `.lhs` extension
 ///     - Current directory contains a `stack.yaml` file
 pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
-    let is_haskell_project = context
+  let is_haskell_project = context
         .try_begin_scan()?
         .set_files(&["stack.yaml"])
+        .set_extensions(&["cabal", "hs", "lhs"])
         .is_match();
 
     if !is_haskell_project {
         return None;
     }
 
-    let haskell_version = utils::exec_cmd(
-        "stack",
-        &["ghc", "--", "--numeric-version", "--no-install-ghc"],
-    )?
-    .stdout;
-    let formatted_version = format_haskell_version(&haskell_version)?;
-
     let mut module = context.new_module("haskell");
     let config: HaskellConfig = HaskellConfig::try_load(module.config);
     module.set_style(config.style);
 
+    let formatted_version = format!("v{}", get_haskell_version()?.trim());
     module.create_segment("symbol", &config.symbol);
     module.create_segment("version", &SegmentConfig::new(&formatted_version));
 
     Some(module)
 }
 
-fn format_haskell_version(haskell_version: &str) -> Option<String> {
-    let formatted_version = format!("v{}", haskell_version.trim());
-    Some(formatted_version)
+fn get_haskell_version() -> Option<String> {
+    let ghc = utils::exec_cmd("ghc", &["--numeric-version"]);
+    let stack = || utils::exec_cmd(
+        "stack",
+        &["ghc", "--", "--numeric-version", "--no-install-ghc"],
+    );
+
+    Some(ghc.or_else(stack)?.stdout)
 }
